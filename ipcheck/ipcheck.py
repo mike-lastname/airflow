526bfffa8b6a8f654125065bc698038cde081759
mike-lastname <mikhail.gilyakhov@gmail.com>
2024-11-17 23:12:48 +0300

    получен IP + дата, время

--- ipcheck/ipcheck.py
+++ ipcheck/ipcheck.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 import requests
 
 r = requests.get('https://api.ipify.org')
-print(r.text)
+# print(r.text)
+print(datetime.now().strftime("%d.%m.%Y %H:%M:%S"), r.text)
