de9f64b3cbcc618a70b4d79b1f1de956480e11e7
mike-lastname <mikhail.gilyakhov@gmail.com>
2024-11-17 23:48:50 +0300

    save iplog

--- ipcheck/ipcheck.py
+++ ipcheck/ipcheck.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 import requests
 
-r = requests.get('https://api.ipify.org')
+r = requests.get('https://api.ipify.org').text
 date = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
 
-with open("iplog.txt", "w") as f:
-    f.write(f"{date} {r.text}\n")
+with open("iplog.txt", "a") as f:
+    f.write(f"{date} {r}\n")
