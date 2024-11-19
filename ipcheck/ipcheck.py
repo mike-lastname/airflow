270a43dbc7fab6eb4e2f79a26f3361177eda1819
mike-lastname <mikhail.gilyakhov@gmail.com>
2024-11-19 23:58:07 +0300

    message

--- ipcheck/ipcheck.py
+++ ipcheck/ipcheck.py
@@ -1,8 +1,18 @@
-from datetime import datetime
+from airflow.decorators import dag, task
+from datetime import datetime as dt
 import requests
 
-r = requests.get('https://api.ipify.org').text
-date = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
 
-with open("iplog.txt", "a") as f:
-    f.write(f"{date} {r}\n")
+@dag(schedule="@hourly", start_date=dt(2024, 11, 19), catchup=False)
+def ip_check():
+    @task
+    def get_ip():
+        r = requests.get('https://api.ipify.org').text
+        date = dt.now().strftime("%d.%m.%Y_%H:%M:%S")
+        with open("/opt/airflow/logs/iplog.txt", "a") as f:
+            f.write(f"{date}: {r}\n")
+
+    get_ip()
+
+
+ip_check()
