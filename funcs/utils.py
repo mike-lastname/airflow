51b7d89f194af2b96aef9d0e371c83d2a0f2b4a5
mike-lastname <mikhail.gilyakhov@gmail.com>
2024-12-20 22:00:49 +0300

    refactor get_files_to_dl refactor download_files refactor download_files_util

--- funcs/utils.py
+++ funcs/utils.py
@@ -2,12 +2,12 @@ import random
 import string
 import os
 import pathlib
-from datetime import datetime as dt
 import io
 import pendulum
+import yadisk
 
 
-def random_string_generator(length):
+def random_string_generator(length: int):
     chars = string.ascii_letters + string.digits
     return ''.join(random.choice(chars) for _ in range(length))
 
@@ -25,9 +25,9 @@ def generator():
         yield res_dict
 
 
-def get_files_to_dl(client, prev_start_date):
+def get_files_to_dl(client: yadisk.Client, prev_start_date: pendulum.DateTime, hard_sync: bool):
     with client:
-        if prev_start_date is None:
+        if prev_start_date is None or hard_sync:
             return [i["path"] for i in client.get_files(fields="path")]
         else:
             files_to_dl = []
@@ -37,10 +37,9 @@ def get_files_to_dl(client, prev_start_date):
             return files_to_dl
 
 
-def uploader(task_run_time: str, tz, client, disk_folder, logger):
+def uploader(task_run_time: str, tz: str, client: yadisk.Client, disk_folder: str, logger):
     local_tz = pendulum.timezone(tz)
-    local_time_str = local_tz.convert(pendulum.parse(task_run_time)).strftime("%Y%m%d_%H%M%S")
-    directory_name = local_time_str
+    directory_name = local_tz.convert(pendulum.parse(task_run_time)).strftime("%Y%m%d_%H%M%S")
     n_files = 0
     with client:
         if client.exists(f"{disk_folder}/{directory_name}") is False:
@@ -53,7 +52,7 @@ def uploader(task_run_time: str, tz, client, disk_folder, logger):
     logger.info(f"Files uploaded: {n_files}")
     logger.info(disk_space_info(client))
 
-def random_edit(client):
+def random_edit(client: yadisk.Client):
     with client:
         all_files = [i["path"] for i in client.get_files(fields="path")]
         edit_files = []
@@ -67,40 +66,36 @@ def random_edit(client):
             client.upload(dl, i, overwrite=True)
 
 
-def delete_all(local_folder):
+def delete_all(local_folder: str):
     os.system(f"rm -rf {local_folder}/*")
 
 
-def download_files(client, local_folder, disk_folder, prev_start_date, logger, hard_sync: bool):
+def download_files(client: yadisk.Client, local_folder: str, disk_folder: str, prev_start_date: pendulum.DateTime, logger, hard_sync: bool):
     if hard_sync is True:
         logger.info(f"Hard_sync is ON! Deleting all files in '{local_folder}' "
                     f"and downloading all files from 'disk:/{disk_folder}'")
         delete_all(local_folder)
-        files_to_dl = [i["path"] for i in client.get_files(fields="path")]
-        download_files_util(client, local_folder, files_to_dl)
     else:
         logger.info(f"Hard_sync is OFF! Downloading new and edited files from 'disk:/{disk_folder}'")
-        files_to_dl = get_files_to_dl(client, prev_start_date)
-        download_files_util(client, local_folder, files_to_dl)
+    files_to_dl = get_files_to_dl(client, prev_start_date, hard_sync)
+    download_files_util(client, local_folder, files_to_dl)
 
 
-def download_files_util(client, local_folder, files_to_dl):
+def download_files_util(client: yadisk.Client, local_folder: str, files_to_dl: list):
     for i in files_to_dl:
-        if pathlib.Path(f"{local_folder}/{i.split('/')[2]}").is_dir():
-            client.download(f"{i}", f"{local_folder}/{i.split('/')[2]}/{i.split('/')[3]}")
-        else:
-            pathlib.Path(f"{local_folder}/{i.split('/')[2]}").mkdir()
-            client.download(f"{i}", f"{local_folder}/{i.split('/')[2]}/{i.split('/')[3]}")
+        if pathlib.Path(local_folder, pathlib.Path(i).parent.name).is_dir() is False:
+            pathlib.Path(local_folder, pathlib.Path(i).parent.name).mkdir()
+        client.download(f"{i}", f"{pathlib.Path(local_folder, pathlib.Path(i).parent.name, pathlib.Path(i).name)}", overwrite=True)
 
 
-def size_convert(size_in_bytes):
+def size_convert(size_in_bytes: int):
     sizes = {"KB": 1024, "MB": 1024 * 1024, "GB": 1024 * 1024 * 1024}
     for k, v in sizes.items():
         if size_in_bytes / v < 1024:
             return str(f"{round(size_in_bytes / v, 3)} {k}")
 
 
-def disk_space_info(client):
+def disk_space_info(client: yadisk.Client):
     with client:
         disk_info = client.get_disk_info(fields=["total_space", "used_space"])
         total_space = int(disk_info["total_space"])
